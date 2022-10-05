0fca643ad3cf58c9aaa157ecc1a8973a6deeb15e
Kirti Mulchandani <69420228+KirtiMulchandani@users.noreply.github.com>
2022-10-05 20:20:23 +0530

    Update Reverse Array.cpp

--- Arrays/Reverse Array.cpp
+++ Arrays/Reverse Array.cpp
@@ -1,1 +1,34 @@
 
+#include <bits/stdc++.h>
+using namespace std;
+
+void rvereseArray(int arr[], int i, int j)
+{
+    while (i < j)
+    {
+        swap(arr[i],arr[j]);
+        i++;
+        j--;
+    }
+}    
+ 
+void printArray(int arr[], int n)
+{
+   for (int i = 0; i < n; i++)
+   cout << arr[i]<< " ";
+   cout << endl;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 3, 4, 5, 6};
+     
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printArray(arr, n);
+    rvereseArray(arr, 0, n-1);
+    cout << "Reversed array : \n";
+    printArray(arr, n);
+     
+    return 0;
+}
