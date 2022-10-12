a3c7f77b6bd6c6ce7d2a375d80f0fd4dd3eec3b3
Kirti Mulchandani <69420228+KirtiMulchandani@users.noreply.github.com>
2022-10-12 20:04:56 +0530

    Create Array Subset of another array.cpp

---
+++ Arrays/Array Subset of another array.cpp
@@ -0,0 +1,21 @@
+string isSubset(int a1[], int a2[], int n, int m) {
+    
+    if(n < m)
+        return "No";
+    
+    unordered_map<int, int> mp;
+    
+    for(int i = 0; i < n; i++)
+        mp[a1[i]]++;
+        
+    
+    for(int i = 0; i < m; i++){
+        if(!(mp[a2[i]])){
+            return "No";
+        }
+        else
+            mp[a2[i]]--;
+    }
+    return "Yes";
+    
+}
