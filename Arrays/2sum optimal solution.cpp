1bf1db7d7ab540da9cd348c2fb90936c6d66987a
Kirti Mulchandani <69420228+KirtiMulchandani@users.noreply.github.com>
2022-10-13 09:43:26 +0530

    Create 2sum optimal solution.cpp

---
+++ Arrays/2sum optimal solution.cpp
@@ -0,0 +1,17 @@
+vector<int> twoSum(vector<int>& nums, int target) {
+        
+    vector<int> ans;
+        
+    unordered_map<int, int>m;
+        
+    for(int i = 0; i < nums.size(); i++){
+        int sum = target - nums[i];
+        if(m.find(sum) != m.end()){
+            ans.push_back(m[sum]);
+            ans.push_back(i);
+            return ans;
+        }          
+            m[nums[i]] = i;
+    }
+    return ans;
+}
